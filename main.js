2728ac2a367180a9f96b761bc360b7a0f91ba88a
looker <985115106@qq.com>
2017-10-09 00:41:27 +0800

    doing app

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');//引入文件读取模块
 
-var documentRoot = 'D:/git/HbuildDemo';//需要访问的本地文件的存放目录
+var documentRoot = 'C:/project/nodejs/HbuildDemo';//需要访问的本地文件的存放目录
 
 var server = http.createServer(function (req, res) {
 
