882a4ac455a6fa42deaaa4c651de4b096ff01175
looker <looker@axsoftware.net>
2017-10-12 09:49:10 +0800

    ....

--- main.js
+++ main.js
@@ -31,6 +31,6 @@ var server = http.createServer(function (req, res) {
             res.write(data);
             res.end();}
     });
-}).listen(8889);// 此处的8889是监听的端口号,可以根据自己的需要配置,注意不要和本地的一些应用程序使用的端口号冲突
+}).listen(80);// 此处的8889是监听的端口号,可以根据自己的需要配置,注意不要和本地的一些应用程序使用的端口号冲突
 
  console.log('服务器开启成功');
